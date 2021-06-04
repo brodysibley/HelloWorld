fe68c74b2db924cf8baa43b5c45a7425f92e6552
brodysibley <85354878+brodysibley@users.noreply.github.com>
2021-06-04 09:55:39 -0500

    Add files via upload

---
+++ Brody'sHelloWorld.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Brody_sHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            var currentDate = DateTime.Now;
+            Console.WriteLine($"{Enviroment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+            Console.Write($"{Enviroment.NewLine} press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
